907f45b55faa83feb1c25dcf4a92840853994545
mosyocoder <128539219+mosyocoder@users.noreply.github.com>
2023-12-09 18:35:34 +0300

    delete filter.js, add left side categories

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
-import Filter from "./components/Filter";
 import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<div>
 			<Navbar />
-			<Filter />
 			<Routes>
 				<Route path={"/"} element={<Products />} />
 			</Routes>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,23 +1,53 @@
 import React, { useContext, useEffect, useState } from "react";
 import { CategoryContext } from "../context/CategoryContext";
 import axios from "axios";
+import styled from "styled-components";
+
+const ProductsContainer = styled.div`
+	max-width: 90%;
+	margin: 15px auto 0 auto;
+	display: flex;
+	flex-direction: row;
+`;
+
+const CategoriesDiv = styled.div`
+	width: 15%;
+
+	@media (max-width: 700px) {
+		display: none;
+	}
+`;
+
+const ProductsDiv = styled.div`
+	width: 85%;
+
+	@media (max-width: 700px) {
+		width: 100%;
+	}
+`;
 
 function Products() {
 	const { category } = useContext(CategoryContext);
 	const [data, setData] = useState([]);
-
-	console.log(data);
+	const [cat, setCat] = useState([]);
 
 	useEffect(() => {
-		axios.get("https://fakestoreapi.com/products/category/jewelery").then((res) => setData(res.data));
+		axios.get("https://dummyjson.com/products").then((res) => setData(res.data.products));
+		axios.get("https://dummyjson.com/products/categories").then((res) => setCat(res.data));
 	}, [category]);
 
 	return (
-		<div>
-			{data.map((item) => (
-				<p>{item.category}</p>
-			))}
-		</div>
+		<ProductsContainer>
+			<CategoriesDiv>
+				CategoriesDiv
+				<ul>
+					{cat.map((category, ix) => (
+						<li key={ix}>{category}</li>
+					))}
+				</ul>
+			</CategoriesDiv>
+			<ProductsDiv>ProductsDiv</ProductsDiv>
+		</ProductsContainer>
 	);
 }
 
